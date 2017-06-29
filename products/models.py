cc09484dd2ec34386f32a037790d0dd87e3d83f1
Luthfi <vlootfie@gmail.com>
2017-06-29 21:24:30 +0000

    add price field

--- products/models.py
+++ products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Product(models.Model):
     """docstring for Product."""
     name = models.CharField(max_length=100)
-    
+    price = models.IntegerField()
+
     def __init__(self, arg):
         super(Product, self).__init__()
         self.arg = arg
