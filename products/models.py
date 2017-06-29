4da4fa7dd6eef12b25f922228bb05f498f38dd79
Luthfi <vlootfie@gmail.com>
2017-06-29 21:26:51 +0000

    migrate products app

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Product(models.Model):
     """docstring for Product."""
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
-    stock = models.IntegerField()
+    price = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
 
     def __init__(self, arg):
         super(Product, self).__init__()
