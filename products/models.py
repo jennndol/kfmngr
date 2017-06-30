e66cc066b9e50f9db3ca142a18410f264c8bf3c5
Luthfi <vlootfie@gmail.com>
2017-06-30 18:41:22 +0000

    before starting to code

--- products/models.py
+++ products/models.py
@@ -4,8 +4,8 @@ class Product(models.Model):
     """docstring for Product."""
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
+    detail = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
 
-    def __init__(self, arg):
-        super(Product, self).__init__()
-        self.arg = arg
+    def __str__(self):
+        return self.name
