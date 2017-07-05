f6d5dfdc29b29eedc517c064b3366465dd03d2ac
Luthfi <vlootfie@gmail.com>
2017-07-05 09:46:28 +0000

    show products on its template

--- products/views.py
+++ products/views.py
@@ -3,9 +3,5 @@ from django.http import HttpResponse
 from products.models import Product
 
 def products(request):
-    return render(request, 'products.html')
-
-def Product(request):
-    # TODO: show product by id
-    product = Product.objects.filter(id=id)
-    return HttpResponse(product, content_type="text/plain")
+    products = Product.get_all_products()
+    return render(request, 'products.html', {'products':products})
