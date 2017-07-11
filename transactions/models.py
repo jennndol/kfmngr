25246eb0d6cbfdf9000a3e1ce398674e1a9568f4
Luthfi <vlootfie@gmail.com>
2017-07-11 10:51:57 +0000

    transactions makemigration

---
+++ transactions/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from suppliers.models import Supplier
+from products.models import Product
+
+class Procurement(models.Model):
+    """docstring for Procurement."""
+    product = models.ForeignKey(Product)
+    supplier = models.ForeignKey(Supplier)
+    quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add = True, auto_now = False)
+    updated_at = models.DateTimeField(auto_now_add = False, auto_now = True)
