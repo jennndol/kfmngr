aa425709748afacdb15519d85fa36dcd8bb56ce1
Luthfi <vlootfie@gmail.com>
2017-07-11 11:21:51 +0000

    add __str__ function

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Procurement
 
-# Register your models here.
+admin.site.register(Procurement)
--- transactions/models.py
+++ transactions/models.py
@@ -7,5 +7,9 @@ class Procurement(models.Model):
     product = models.ForeignKey(Product)
     supplier = models.ForeignKey(Supplier)
     quantity = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add = True, auto_now = False)
     updated_at = models.DateTimeField(auto_now_add = False, auto_now = True)
+
+    def __str__(self):
+        return self.product
