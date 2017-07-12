d1bd3d3c353a9c681d444a962276950327b7b3d2
Luthfi <vlootfie@gmail.com>
2017-07-12 14:22:55 +0000

    create create view for adding some product

--- transactions/views.py
+++ transactions/views.py
@@ -10,3 +10,15 @@ def buying(request):
 def create(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
+        if form.is_valid():
+            product = Product()
+            product.name = form.cleaned_data.get('name')
+            product.price = form.cleaned_data.get('price')
+            product.detail = form.cleaned_data.get('detail')
+            product.weight = form.cleaned_data.get('weight')
+            product.stock = form.cleaned_data.get('stock')
+            product.save()
+            return redirect('/transaction/')
+    else:
+        form = ArticleForm()
+    return render(request, 'create.html', {'form': form})
