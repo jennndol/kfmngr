cc8cb81586ea6bc79f0fa369c2aec7badba49f76
Luthfi <vlootfie@gmail.com>
2017-07-14 11:17:15 +0000

    update stock success

--- transactions/models.py
+++ transactions/models.py
@@ -12,6 +12,13 @@ class Procurement(models.Model):
     updated_at = models.DateTimeField(auto_now_add = False, auto_now = True)
 
     def save(self, *args, **kwargs):
+        print("Procurement save method is being called")
+        product = Product.objects.get(id=self.product.id)
+        print("before update : " + str(product.stock))
+        product.stock = product.stock + self.quantity
+        print("after update : " + str(product.stock))
+        product.save()
+        # TODO: update stock in products table
         super(Procurement, self).save(*args, **kwargs)
 
     def __str__(self):
