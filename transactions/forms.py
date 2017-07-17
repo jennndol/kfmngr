bb0ad409f9e2749aac0da2a9ca3a3a5533995c59
Luthfi <vlootfie@gmail.com>
2017-07-17 22:38:25 +0700

    edit new_buying view

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 
 from .models import Procurement
+
+
+class ProcurementForm(forms.ModelForm):
+    
+    class Meta:
+        model = Procurement
+        fields = ['product', 'supplier', 'quantity', 'price']
--- transactions/views.py
+++ transactions/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from transactions.models import Procurement
+from transactions.forms import ProcurementForm
 
 def buying(request):
     procurements = Procurement.objects.all()
     return render(request, 'buying.html', {'procurements':procurements})
 
 def new_buying(request):
-    return render(request, 'new_procurement.html')
+    if request.method == 'POST':
+        form = ProcurementForm(request.POST)
+        if form.is_valid()
+        procurement = Procurement()
+        procurement.product = form.cleaned_data.get('product')
+        procurement.supplier = form.cleaned_data.get('supplier')
+        procurement.quantity = form.cleaned_data.get('quantity')
+        procurement.price = form.cleaned_data.get('price')
+        procurement.user = request.user
+        procurement.save()
+        return redirect('/buying/')
+
+    else:
+        form = ProcurementForm()
+    return render(request, 'new_procurement.html', {'form':form})
