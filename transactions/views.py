359b9543f2156436e6a4149a0d41ca4d9d3fb76c
Luthfi <vlootfie@gmail.com>
2017-07-17 22:23:51 +0700

    add test view for new_buying view

--- transactions/views.py
+++ transactions/views.py
@@ -4,3 +4,6 @@ from transactions.models import Procurement
 def buying(request):
     procurements = Procurement.objects.all()
     return render(request, 'buying.html', {'procurements':procurements})
+
+def new_buying(request):
+    return render(request, 'new_procurement.html')
