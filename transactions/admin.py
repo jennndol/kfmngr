1df058eaaa8579882aba6a4fd21800427b718c70
Luthfi <vlootfie@gmail.com>
2017-07-17 21:52:06 +0700

    add models to admin.py

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Procurement
+from .models import Procurement, Selling, Detail
 
 admin.site.register(Procurement)
+admin.site.register(Selling)
+admin.site.register(Detail)
