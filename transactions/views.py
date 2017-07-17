9ff55acfe00a2254c53b59b803f719a98f37bba1
Luthfi <vlootfie@gmail.com>
2017-07-17 22:48:10 +0700

    new_buying view edit and add gitignore

--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from transactions.models import Procurement
 from transactions.forms import ProcurementForm
 
@@ -9,16 +9,18 @@ def buying(request):
 def new_buying(request):
     if request.method == 'POST':
         form = ProcurementForm(request.POST)
-        if form.is_valid()
-        procurement = Procurement()
-        procurement.product = form.cleaned_data.get('product')
-        procurement.supplier = form.cleaned_data.get('supplier')
-        procurement.quantity = form.cleaned_data.get('quantity')
-        procurement.price = form.cleaned_data.get('price')
-        procurement.user = request.user
-        procurement.save()
-        return redirect('/buying/')
+        if form.is_valid():
+            procurement = Procurement()
+            procurement.product = form.cleaned_data.get('product')
+            procurement.supplier = form.cleaned_data.get('supplier')
+            procurement.quantity = form.cleaned_data.get('quantity')
+            procurement.price = form.cleaned_data.get('price')
+            procurement.user = request.user
+            procurement.save()
+            return redirect('/buying/')
 
     else:
         form = ProcurementForm()
     return render(request, 'new_procurement.html', {'form':form})
+
+# TODO: make receipt procurement, one receipt for many products and one supplier
