cef40ec653cf0a3bd0de764332a158988c5e277e
Luthfi <vlootfie@gmail.com>
2017-07-19 10:09:10 +0000

    create SellingForm

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
-from .models import Procurement
+from .models import Procurement, Selling
 
 
 class ProcurementForm(forms.ModelForm):
-    
+
     class Meta:
         model = Procurement
         fields = ['product', 'supplier', 'quantity', 'price']
+
+class SellingForm(forms.ModelForm):
+    class Meta:
+        model = Selling
+        fields = ['buyer']
