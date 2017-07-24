d813676493769b5ec3487c2146d9057767f63106
Luthfi <vlootfie@gmail.com>
2017-07-24 20:42:54 +0700

    import get_object_or_404 to views.py

--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from transactions.models import Procurement, Selling
 from transactions.forms import ProcurementForm
 from transactions.forms import SellingForm
