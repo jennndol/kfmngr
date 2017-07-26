233838f4266e22b11ebb4a06744d64d3a1e06af3
Luthfi <vlootfie@gmail.com>
2017-07-26 22:42:51 +0700

    add get_semua_produk test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Produk
+
+class ProdukModelTests(TestCase):
+    def test_get_semua_produk(self):
+        semua_produk_1 = Produk.objects.all()
+        semua_produk_2 = Produk.get_semua_produk()
+        if semua_produk_1.count() == 0:
+            print("There's no record inside the table")
+        else:
+            self.assertEqual(semua_produk_1, semua_produk_2)
